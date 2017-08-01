dd80885598a1a43c8da2ef626b4dd577f39daff3
ClementRolinat <clement.rolinat@ha.ax>
2017-08-01 16:44:51 +0300

    tests

--- path-planning-mov.cpp
+++ path-planning-mov.cpp
@@ -115,7 +115,7 @@ void pavingMov(IntervalVector speed, vector<SepInter*> listSep, vector<IntervalV
 int main(int argc, char** argv) {
     Interval T(0,30);
 
-    vector<vector<double>> border = {{-150, 0},{0, 150},{150, 0},{0, -150}};
+    vector<vector<double>> border = {{-150, 0},{0, 150},{140, 0}, {200,0},{0, -150}};
 
     double pos[2][2] ={{-1,1},{-1,1}};
     IntervalVector boatInitPos(2, pos);
@@ -129,8 +129,8 @@ int main(int argc, char** argv) {
     double pos2[4][2] = {{3, 3.1}, {10, 11}, {0, 1}, {2.0, 2.2}};
     IntervalVector obs1(4, pos1);
     IntervalVector obs2(4, pos2);
-    obstacles.push_back(obs1);
-    obstacles.push_back(obs2);
+    //obstacles.push_back(obs1);
+    //obstacles.push_back(obs2);
 
 
 
@@ -149,6 +149,7 @@ int main(int argc, char** argv) {
     SepFwdBwd* pSep1;
     SepFwdBwd* pSep2;
     SepFwdBwd* pSep3;
+    SepFwdBwd* pSep4;
     SepInter* pSep;    
 
     
@@ -164,9 +165,17 @@ int main(int argc, char** argv) {
                                                     ((border[(i+1)%border.size()][0] - border[i][0])*(border[i][1] - (vy*T.ub() + boatInitPos[1])) - 
                                                     (border[(i+1)%border.size()][1] - border[i][1])*(border[i][0] - (vx*T.ub() + boatInitPos[0])))
                                     );
-        
+        /*
+        pf3 = new Function(vx, vy, ((boatInitPos[0] | (vx*T.ub()+boatInitPos[0])) & (border[i][0] | border[(i+1)%border.size()][0])).size());
+        pf4 = new Function(vx, vy, ((boatInitPos[1] | (vy*T.ub()+boatInitPos[1])) & (border[i][1] | border[(i+1)%border.size()][1])).size());
+        */
+
         pSep1 = new SepFwdBwd(*pf1, LEQ);
         pSep2 = new SepFwdBwd(*pf2, LEQ);
+        /*
+        pSep3 = new SepFwdBwd(*pf3, GT);
+        pSep4 = new SepFwdBwd(*pf4, GT);
+        */
         pSep = new SepInter(*pSep1, *pSep2);
 
         listSep.push_back(pSep);
